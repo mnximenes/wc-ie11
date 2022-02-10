2e1e988bd83441b50334b28c2cc3c092b2540172
Hemant Kapoor <Hemant.Kapoor@motoinsight.com>
2022-02-10 13:09:22 -0500

    fix

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,5 +1,5 @@
 // import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
-
+import { legacyPlugin } from '@web/dev-server-legacy';
 /** Use Hot Module replacement by adding --hmr to the start command */
 const hmr = process.argv.includes('--hmr');
 
@@ -20,6 +20,21 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   plugins: [
     /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
     // hmr && hmrPlugin({ exclude: ['**/*/node_modules/**/*'], presets: [presets.litElement] }),
+    legacyPlugin({
+      polyfills: {
+        webcomponents: true,
+        // Inject lit's polyfill-support module into test files, which is required
+        // for interfacing with the webcomponents polyfills
+        custom: [
+          {
+            name: 'lit-polyfill-support',
+            path: 'node_modules/lit/polyfill-support.js',
+            test: "!('attachShadow' in Element.prototype)",
+            module: false,
+          },
+        ],
+      },
+    }),
   ],
 
   // See documentation for all available options
